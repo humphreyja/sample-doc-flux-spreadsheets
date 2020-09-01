13153ffae589e1746b15fad5d3deac21c2bdc4ff
Jake Humphrey <jakehumphrey18@gmail.com>
2020-09-01 17:46:54 -0500

    updating doc flux and pdf stuff

--- src/documents/ExamplePDFDoc.js
+++ src/documents/ExamplePDFDoc.js
@@ -4,22 +4,43 @@ import { Document as PDFDocument } from '@harvest-profit/doc-flux-pdfs';
 import Comp from './components/PDFHeadings';
 // import Comp from './components/SampleTable';
 
+const publicURL = `${window.location.protocol}//${window.location.host}`;
+
+const style = {
+  fonts: {
+    OpenSans: {
+      normal: `${publicURL}/Open_Sans/OpenSans-Regular.ttf`,
+      bold: `${publicURL}/Open_Sans/OpenSans-Bold.ttf`,
+      italics: `${publicURL}/Open_Sans/OpenSans-Italic.ttf`,
+      bolditalics: `${publicURL}/Open_Sans/OpenSans-BoldItalic.ttf`,
+    },
+    Lobster: {
+      normal: `${publicURL}/Lobster/Lobster-Regular.ttf`,
+    },
+  },
+  css: {
+    year: {
+      color: 'blue',
+      font: 'Lobster',
+      bold: false,
+    },
+    numberCol: {
+      alignment: 'right',
+    },
+  },
+  default: {
+    font: 'OpenSans',
+    fontSize: 10,
+    color: '#333333',
+  },
+};
 
 export default class ExampleDoc extends PDFDocument {
   static propTypes = {
     name: PropTypes.string.isRequired,
   }
 
-  static styleSheet() {
-    return {
-      year: {
-        color: 'blue',
-      },
-      numberCol: {
-        alignment: 'right',
-      },
-    };
-  }
+  static documentStyling = style;
 
   static documentSettings(props) {
     return {
